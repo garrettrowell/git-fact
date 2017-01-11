f008631316087e6ad3011c6982b65bd7eb86281d
Garrett Rowell <garrett2@cnwr.com>
2017-01-11 16:04:18 -0500

    return undef if git is not installed rather than throwing errors

--- lib/facter/git_version.rb
+++ lib/facter/git_version.rb
@@ -1,7 +1,9 @@
 Facter.add(:git_version) do
   setcode do
-    git_version = Facter::Util::Resolution.exec('git --version 2>&1')
-    Facter.debug "Matching git '#{git_version}'"
-    %r{^git version (\d+\.\d+(\.\d+)?(\.\d+)?)}.match(git_version)[1]
+    if Facter::Util::Resolution.which('git')
+      git_version = Facter::Util::Resolution.exec('git --version 2>&1')
+      Facter.debug "Matching git '#{git_version}'"
+      %r{^git version (\d+\.\d+(\.\d+)?(\.\d+)?)}.match(git_version)[1]
+    end
   end
 end
